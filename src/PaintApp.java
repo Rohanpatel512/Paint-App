0ee0ee2fb612c7f85a65ecf5913d8e294dd04f94
rohan <pater8886@wrdsb.ca>
2020-05-27 18:23:43 -0400

    A simple paint app project I have created. Feedback on documentation, readability and etc. Would be amazing!

---
+++ src/PaintApp.java
@@ -0,0 +1,11 @@
+
+public class PaintApp  {
+    
+    // Class Variables
+    public static void main(String[] args) {
+        
+        // Create an instance of a layout class 
+        LayoutApp paintApp = new LayoutApp();
+       
+    }
+}
